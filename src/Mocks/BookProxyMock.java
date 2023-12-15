d2a05b45db0a285173b65e17368467038bf7d2d4
Fabiona Tafciu <106432529+ftafciu@users.noreply.github.com>
2023-12-15 13:11:48 +0100

    Created BookProxyMock.java

---
+++ src/Mocks/BookProxyMock.java
@@ -0,0 +1,24 @@
+package Mocks;
+
+import Products.Book;
+import Products.BookDb;
+
+import java.util.ArrayList;
+
+public class BookProxyMock implements BookDb {
+
+    private ArrayList<Book> books;
+
+    public BookProxyMock(ArrayList<Book> books){
+        this.books = books;
+    }
+    @Override
+    public void writeBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public ArrayList<Book> readBooks() {
+        return books;
+    }
+}
