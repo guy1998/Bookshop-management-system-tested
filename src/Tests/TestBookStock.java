0c38482f5e2dd09619ae6cbc09efdf06a80c27e5
Fabiona Tafciu <106432529+ftafciu@users.noreply.github.com>
2023-12-15 13:12:42 +0100

    Started testing bookstock

--- src/Tests/TestBookStock.java
+++ src/Tests/TestBookStock.java
@@ -1,4 +1,69 @@
 package Tests;
 
+import Exceptions.InvalidBookInfo;
+import Mocks.BookProxyMock;
+import Products.Author;
+import Products.Book;
+import Products.BookStock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestBookStock {
+
+    private ArrayList<Book> books = new ArrayList<Book>();
+    private ArrayList<Book> runningLow = new ArrayList<>();
+
+    @BeforeEach
+    public void createData(){
+        try {
+            Book book1 = new Book("132-2141-421", "Harry Potter", "Fantasy", 13.5, 14.5, 15.5, 1, 1, 1991, new Author("Joanne", "K", "Rowling"));
+            Book book2 = new Book("132-2241-421","Hobbit",  "Fantasy", 13.5, 14.5, 15.5, 1, 1, 1991, new Author("John", "Ronald", "Tolkien"));
+            Book book3 = new Book("132-2341-421","Alice in the Wonderland",  "Fantasy", 13.5, 14.5, 15.5, 1, 1, 1991, new Author("Lewis", "Carol"));
+            book1.addNumber(6);
+            book2.addNumber(3);
+            book3.addNumber(8);
+            books.add(book1);
+            books.add(book2);
+            books.add(book3);
+            runningLow.add(book2);
+        } catch (InvalidBookInfo e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testRunningLowWithNoBooks(){
+        ArrayList<Book> dataset = new ArrayList<>();
+        BookStock stock = new BookStock(new BookProxyMock(dataset));
+        assertEquals(new ArrayList<Book>(), stock.runningLow());
+    }
+
+    @Test
+    public void testRunningLowWithDataButNoBookRunningLow(){
+        books.remove(1);
+        BookStock stock = new BookStock(new BookProxyMock(books));
+        assertEquals(new ArrayList<Book>(), stock.runningLow());
+    }
+
+    @Test
+    public void testRunningLowWithOneBookRunningLow(){
+        BookStock stock = new BookStock(new BookProxyMock(books));
+        assertEquals(runningLow, stock.runningLow());
+    }
+
+    @Test
+    public void testRunningLowWithAllBooksRunningLow(){
+        books.get(0).addNumber(-3);
+        books.get(2).addNumber(-4);
+        runningLow.add(books.get(0));
+        runningLow.add(books.get(2));
+        BookStock stock = new BookStock(new BookProxyMock(books));
+        assertEquals(runningLow, stock.runningLow());
+    }
+
+
 }
